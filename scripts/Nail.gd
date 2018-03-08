0b5a2402df7563fe159c5cd741fac542feea9d72
Guilherme Oliveira <htoguilherme@gmail.com>
2018-03-08 03:16:46 -0300

    Nail scene

---
+++ scripts/Nail.gd
@@ -0,0 +1,16 @@
+extends Area
+
+onready var animation = $Animation
+
+var already_hit = false
+
+func _ready():
+	connect("area_entered", self, "_on_area_entered")
+	pass
+
+
+func _on_area_entered(area):
+	if area.name == "Hammer" and !already_hit:
+		animation.play("hit")
+
+
